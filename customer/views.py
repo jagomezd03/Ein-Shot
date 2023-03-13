16913a4437d2e518cf944797a3e9a2fa67af86e5
jagomezd03 <jagomezd@eafit.edu.co>
2023-03-13 01:21:40 -0500

    v0.3

---
+++ customer/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return render(request, 'home.html')
+def home_es(request):
+    return render(request, 'es/main.html')
+def menu_es(request):
+    return render(request, 'es/menu.html')
+def sessions_es(request):
+    return render(request, 'es/session.html')
+def home_en(request):
+    return render(request, 'en/main.html')
+def menu_en(request):
+    return render(request, 'en/menu.html')
+def sessions_en(request):
+    return render(request, 'en/session.html')
---
+++ einshot/urls.py
@@ -0,0 +1,29 @@
+"""einshot URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from customer import views as customerViews
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',customerViews.index),
+    path('es/home',customerViews.home_es),
+    path('es/menu',customerViews.menu_es),
+    path('es/login',customerViews.sessions_es),
+    path('en/home',customerViews.home_en),
+    path('en/menu',customerViews.menu_en),
+    path('en/login',customerViews.sessions_en),
+]
