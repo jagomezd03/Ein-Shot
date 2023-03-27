e548efe4115df5ee5923be2d4d56274be89559e4
jagomezd03 <jagomezd@eafit.edu.co>
2023-03-26 23:38:27 -0500

    Age verification solved

--- customer/static/js/verification.js
+++ customer/static/js/verification.js
@@ -1,42 +1,50 @@
 import { db } from "./firebase.js";
 import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js"
 import { showMessage } from "./showMessage.js";
+const datetime = new Date()
 const regForm = document.querySelector('#regForm')
 
-function verifyAge(day, month, year) {
-    return new Date(year+18, month-1, day) <= new Date();
+//Function to solve a problem related with the timezone on the input
+function correction(date){
+    const d = date.getTime() +18000000
+    const nd = new Date(d)
+    return nd
+}
+
+function verifyAge(date){
+    const yd = date.getFullYear();const md = date.getMonth()+1;const dd = date.getDate();
+    return new Date(yd+18, md-1, dd) <= new Date();
 }
 
 regForm.addEventListener('submit', async e =>{
     e.preventDefault()
     //Getting the values
-    const birthdate = regForm['birthdate'].value
+    const dateInput = new Date(regForm['birthdate'].value)
+    const d = correction(dateInput)
+    const weekday = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
+    let day = weekday[datetime.getDay()]
     const gender = regForm['gender'].value
-    //Formatting the data
-    const y = birthdate.getFullYear();
-    const m = birthdate.getMonth() + 1;
-    const d = birthdate.getDate()
-    const time = datetime.getHours()+':'+datetime.getMinutes()+':'+datetime.getSeconds();
-    //Storing value,the function returned
-    const v = verifyAge(d,m,y)
     //Timestamp
-    const datetime = new Date()
-    const date = datetime.getFullYear()+"/"+datetime.getMonth()+"/"+datetime.getDate()+" "+time
-    if(v==='true'){
+    const c = verifyAge(d,datetime)
+    console.log(c)
+    if(c){
+        const date = d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear()
+        const datetimestamp = day+", "+datetime.getFullYear()+"/"+(datetime.getMonth()+1)+"/"+datetime.getDate()+" "+datetime.getHours()+':'+datetime.getMinutes()+':'+datetime.getSeconds();
         try {
             const docRef = await addDoc(collection(db, "users"), {
-                birthdate: birthdate,
+                birthdate: date,
                 gender: gender,
-                datetime: date
+                datetime: datetimestamp
             });
             console.log("Document written with ID: ", docRef.id);
-            setTimeout(function(){
-                window.location.href = "";
-            }, 500); 
+            setTimeout(() => {
+                window.location.href = "/es/menu";
+            }, 500);
         } catch (error) {
-            showMessage("Something went wrong: "+error.code, "error");
+            showMessage("Something went wrong: " + error.code, "error");
         }
-    } else {
-        showMessage("+18 Years Only")
+    } else{
+        showMessage("+18 years old only", "error")
     }
+    
 })
